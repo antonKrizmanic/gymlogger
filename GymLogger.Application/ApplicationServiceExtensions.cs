374e993377eef2fd6fc87b31d0d7db45d272db7c
Toni <anton_krizmanic@hotmail.com>
2024-05-25 15:30:51 +0200

    Added auto mapper and profiles

--- GymLogger.Application/ApplicationServiceExtensions.cs
+++ GymLogger.Application/ApplicationServiceExtensions.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace GymLogger.Application;
-public static class ServiceExtensions
+public static class ApplicationServiceExtensions
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
