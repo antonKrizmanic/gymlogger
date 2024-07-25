2183c92533e211d7020610302d37fd99d40ac619
Toni <anton_krizmanic@hotmail.com>
2024-07-25 21:03:26 +0200

    Initial exercise detail page

--- GymLogger.Api/GymLogger.Api.Client/Pages/Exercises/Detail.razor.cs
+++ GymLogger.Api/GymLogger.Api.Client/Pages/Exercises/Detail.razor.cs
@@ -1,8 +1,17 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using GymLogger.Shared.Models.Exercise;
+using GymLogger.Shared.Services;
+using Microsoft.AspNetCore.Components;
 
 namespace GymLogger.Api.Client.Pages.Exercises;
 
 public partial class Detail : ComponentBase
 {
     [Parameter] public Guid Id { get; set; }
+    [Inject] private IExerciseApiService ExerciseApiService { get; set; }
+    private ExerciseDto Exercise { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        this.Exercise = await ExerciseApiService.GetAsync(Id);
+    }
 }
