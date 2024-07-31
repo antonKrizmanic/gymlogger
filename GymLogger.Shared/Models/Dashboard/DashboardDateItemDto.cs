e09acf1b913a5f24cac88747c7410d0b5deebfce
Toni <anton_krizmanic@hotmail.com>
2024-07-31 20:52:57 +0200

    Remove not used prop

--- GymLogger.Shared/Models/Dashboard/DashboardDateItemDto.cs
+++ GymLogger.Shared/Models/Dashboard/DashboardDateItemDto.cs
@@ -3,7 +3,6 @@ public class DashboardDateItemDto
 {
     public DateOnly Date { get; set; }
     public decimal? Weight { get; set; }
-    public string WeightString => Weight?.ToString("0.##");
     public decimal? Series { get; set; }
     public decimal? Reps { get; set; }
 }
