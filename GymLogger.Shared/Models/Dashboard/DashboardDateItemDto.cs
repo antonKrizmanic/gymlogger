1c9910c2afc167afca1c36771ad4f7a9d020fa23
Toni <anton_krizmanic@hotmail.com>
2024-07-31 20:44:12 +0200

    Fix Weight graph

--- GymLogger.Shared/Models/Dashboard/DashboardDateItemDto.cs
+++ GymLogger.Shared/Models/Dashboard/DashboardDateItemDto.cs
@@ -3,6 +3,7 @@ public class DashboardDateItemDto
 {
     public DateOnly Date { get; set; }
     public decimal? Weight { get; set; }
+    public string WeightString => Weight?.ToString("0.##");
     public decimal? Series { get; set; }
     public decimal? Reps { get; set; }
 }
